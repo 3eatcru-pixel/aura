87beb126808133e790fedd3b3033260876b611da
3eatcru-pixel <3eatcru@gmail.com>
2026-05-14 20:14:15 -0300

    versel plugin
    versel

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
+import vercel from 'vite-plugin-vercel';
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss(), vercel()],
     define: {
     },
     resolve: {
